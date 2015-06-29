219fa67aa114cba3d08cc7eb16619e2921c98e80
0xack13 <0xack13@gmail.com>
2015-06-29 16:20:31 +0300

    App routes

--- app.rb
+++ app.rb
@@ -4,3 +4,20 @@ require 'sinatra/activerecord'
 set :database, {adapter: "sqlite3", database: "foo.sqlite3"}
 
 
+class ShortenedUrl < ActiveRecord::Base
+	validates_uniqueness_of :url
+	validates_presence_of :url
+	validates_format_of :url, :with => /^\b((?:https?:\/\/)(?:[^\s()<>]+|(([^\s()<>]+|(([^\s()<>]+)))))+(?:(([^\s()<>]+|(([^\s()<>]+))))|[^\s`!()[]{};:'".,<>?«»“”‘’]))$/ 
+end
+
+get '/' do
+	
+end
+
+post '/' do
+end
+
+get '/:shortened' do
+	short_url = ShortenedUrl.find(params[:shortened])
+	redirect short_url.url
+end
