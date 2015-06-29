d7704efcf5cc329e0c678ea34d50ca9d5e2be751
0xack13 <0xack13@gmail.com>
2015-06-29 17:25:12 +0300

    Working shortner

--- app.rb
+++ app.rb
@@ -7,14 +7,20 @@ set :database, {adapter: "sqlite3", database: "foo.sqlite3"}
 class ShortenedUrl < ActiveRecord::Base
 	validates_uniqueness_of :url
 	validates_presence_of :url
-	validates_format_of :url, :with => /^\b((?:https?:\/\/)(?:[^\s()<>]+|(([^\s()<>]+|(([^\s()<>]+)))))+(?:(([^\s()<>]+|(([^\s()<>]+))))|[^\s`!()[]{};:'".,<>?«»“”‘’]))$/ 
+  	validates_format_of :url, :with => /^\b((?:https?:\/\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))$/, :multiline => true
 end
 
 get '/' do
-	
+	haml :index
 end
 
 post '/' do
+	@short_url = ShortenedUrl.find_or_create_by(:url => params[:url])
+	if @short_url.valid?
+		haml :success
+	else
+		haml :index
+	end
 end
 
 get '/:shortened' do
